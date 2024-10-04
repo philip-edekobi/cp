f18212f1c6660556b53382076510b70c4f9ca9c0
Philip Edekobi <philipedekobi@gmail.com>
2024-10-04 16:23:27 +0100

    docs left for this week

--- backend/src/routes/contributionRoutes.ts
+++ backend/src/routes/contributionRoutes.ts
@@ -1,5 +1,43 @@
 import { Router } from "express";
+import {
+  newContribution,
+  oneContribution,
+  editContribution,
+  allContributions,
+  allParishContributions,
+  newContributionType,
+  oneContributionType,
+  editContributionType,
+  deleteContributionType,
+  allContributionTypes,
+  parishContributionTypes,
+} from "../controllers/contribution";
+import { auth, isParishAdmin, isAdmin } from "../middleware/auth";
 
 const router = Router();
+
+router.get("/all", auth, isAdmin, allContributions);
+
+router.get("", auth, allParishContributions);
+
+router.get("/:id", auth, oneContribution);
+
+router.post("", auth, isParishAdmin, newContribution);
+
+router.patch("/:id", auth, editContribution);
+
+// contribution type routes
+
+router.get("/type/all", auth, isAdmin, allContributionTypes);
+
+router.get("/type", auth, parishContributionTypes);
+
+router.get("/type/:id", auth, oneContributionType);
+
+router.post("/type", auth, isParishAdmin, newContributionType);
+
+router.patch("/type/:id", auth, auth, editContributionType);
+
+router.delete("/type/:id", auth, deleteContributionType);
 
 export default router;
--- backend/src/routes/inventoryRoutes.ts
+++ backend/src/routes/inventoryRoutes.ts
@@ -1,5 +1,43 @@
 import { Router } from "express";
+import {
+  addAssetToInventory,
+  getAsset,
+  getParishInventory,
+  editAsset,
+  deleteAsset,
+  allInventoryCategories,
+  allInventoryCategoriesForParishAdmin,
+  newInventoryCategory,
+  inventoryCategoryByID,
+  editInventoryCategory,
+  deleteInventoryCategory,
+} from "../controllers/inventory";
+import { auth, isAdmin, isParishAdmin } from "../middleware/auth";
 
 const router = Router();
+
+router.get("", auth, getParishInventory);
+
+router.get("/asset/:id", auth, getAsset);
+
+router.post("", auth, isParishAdmin, addAssetToInventory);
+
+router.patch("/asset/:id", auth, isParishAdmin, editAsset);
+
+router.delete("/asset/:id", auth, isParishAdmin, deleteAsset);
+
+// category routes
+
+router.get("/category/all", auth, isAdmin, allInventoryCategories);
+
+router.get("/category", auth, allInventoryCategoriesForParishAdmin);
+
+router.post("/category", auth, isParishAdmin, newInventoryCategory);
+
+router.get("/category/:id", auth, inventoryCategoryByID);
+
+router.patch("/category/:id", auth, isParishAdmin, editInventoryCategory);
+
+router.delete("/category/:id", auth, isParishAdmin, deleteInventoryCategory);
 
 export default router;
